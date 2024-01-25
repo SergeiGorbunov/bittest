0fe4086840137135a8280fb112c772cfc150ae5c
SergeyGorbuov <fantasycraft2000@gmail.com>
2024-01-25 21:27:43 +0300

    fix

--- src/widjets/Drawer/Drawer.jsx
+++ src/widjets/Drawer/Drawer.jsx
@@ -18,7 +18,7 @@ const Drawer = () => {
         setIsOpen(!isOpen);
       }}
       className={
-        isOpen ? styles.drawer__container : styles["drawer__container-hidden"] + " " + styles.drawer
+        styles.drawer + " " + isOpen ? styles.drawer__container : styles["drawer__container-hidden"]
       }
     >
       <div onClick={(e) => e.stopPropagation()} className={styles.drawer}>
