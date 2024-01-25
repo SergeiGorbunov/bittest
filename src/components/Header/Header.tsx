974006e822ec324b1258fd84829145b3093baf00
SergeyGorbuov <fantasycraft2000@gmail.com>
2024-01-25 21:05:49 +0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,10 +5,8 @@ const Header: React.FC = () => {
       <h2>BitTest</h2>
       <p className={styles.company}>Моя организация</p>
       <div className={styles.profile}>
-        <img
+        <div
           className={styles.profile__avatar}
-          src='../../assets/images/default-avatar.png'
-          alt="avatar"
         />
         <div className={styles.profile__text}>
           <p className={styles.profile__meta}>Вы авторизованы</p>
